16af650efdf50479cdb1c150797a795e6ed4c0df
Peter <peter@infratron.io>
2019-05-03 10:23:38 +0200

    :hammer: Added indexing bash script

---
+++ docker/indexer/index.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+file_data() {
+    cat <<EOF
+{
+    "filename": "${file##*/}",
+    "height": "${height:-0}",
+    "width": "${width:-0}",
+    "size": "${size:-onbekend}",
+    "time": "${time:-0}",
+    "camera": "${camera:-onbekend}"
+}
+EOF
+}
+
+generate_mapping() {
+    cat <<EOF
+{
+  "mappings": {
+    "_doc": {
+      "properties": {
+        "filename": { "type": "keyword" },
+        "height": { "type": "integer" },
+        "width": { "type": "integer" },
+        "size": { "type": "keyword" },
+        "time": { "type": "date" },
+        "camera": { "type": "keyword" }
+      }
+    }
+  }
+}
+EOF
+}
+
+DIRECTORY=../../data/bucket/
+
+printf "Deleting index \n"
+curl -H "Content-Type: application/json" -XDELETE "http://localhost:9200/fotoindex" 
+printf "Creating index \n";
+curl -H "Content-Type: application/json" -XPUT "http://localhost:9200/fotoindex?include_type_name=true" -d "$(generate_mapping)"
+printf "\n"
+printf "Starting index \n"
+for file in ${DIRECTORY}*
+do
+    if file "$file" |grep -qE 'image|bitmap'; then
+        hash=$(sha1sum $file)
+        printf "Indexing $file with index $hash \n"
+        
+        #image data
+        height=`identify -format "%h" $file`
+        width=`identify -format "%w" $file`
+        size=`identify -format "%b" $file`
+        time=`identify -format "%[EXIF:DateTime]" $file`
+                echo $time
+
+        camera=`identify -format "%[EXIF:Make] %[EXIF:Model]" $file`
+
+        curl -H "Content-Type: application/json" -XPOST "http://localhost:9200/fotoindex/_doc/$hash_" -d "$(file_data)"
+        printf "\n"
+    fi
+done
+
+
+
