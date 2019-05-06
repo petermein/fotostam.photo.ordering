241508eb137037c538d70c54abbdaa9b09ef060a
Peter <peter@infratron.io>
2019-05-06 14:06:11 +0200

    :hammer: Now with inotify recursive

--- docker/indexer/index.sh
+++ docker/indexer/index.sh
@@ -32,36 +32,23 @@ generate_mapping() {
 EOF
 }
 
-DIRECTORY=/data/bucket/
+directory=/data/bucket
 
-while true
+while read path action file
 do
+  printf "Test: $file"
+  if file "$directory/$file" |grep -qE 'image|bitmap'; then
+    hash=$(sha1sum $directory/$file)
+    printf "Indexing $directory/$file with index $hash \n"
+    
+    #image data
+    height=`identify -format "%h" "$directory/$file"`
+    width=`identify -format "%w" $directory/$file`
+    size=`identify -format "%b" $directory/$file`
+    time=`identify -format "%[EXIF:DateTime]" $directory/$file`
+    camera=`identify -format "%[EXIF:Make] %[EXIF:Model]" $directory/$file`
 
-  printf "Deleting index \n"
-  # curl -H "Content-Type: application/json" -XDELETE "${ELASTICSEARCH_HOST}fotoindex" 
-  printf "Creating index \n";
-  curl -H "Content-Type: application/json" -XPUT "${ELASTICSEARCH_HOST}fotoindex?include_type_name=true" -d "$(generate_mapping)"
-  printf "\n"
-  printf "Starting index \n"
-  for file in ${DIRECTORY}*
-  do
-      if file "$file" |grep -qE 'image|bitmap'; then
-          hash=$(sha1sum $file)
-          printf "Indexing $file with index $hash \n"
-          
-          #image data
-          height=`identify -format "%h" $file`
-          width=`identify -format "%w" $file`
-          size=`identify -format "%b" $file`
-          time=`identify -format "%[EXIF:DateTime]" $file`
-                  echo $time
-
-          camera=`identify -format "%[EXIF:Make] %[EXIF:Model]" $file`
-
-          curl -H "Content-Type: application/json" -XPOST "${ELASTICSEARCH_HOST}fotoindex/_doc/$hash_" -d "$(file_data)"
-          printf "\n"
-      fi
-  done
-  sleep 1m;
-done
-
+    curl -H "Content-Type: application/json" -XPOST "${ELASTICSEARCH_HOST}fotoindex/_doc/$hash_" -d "$(file_data)"
+    printf "\n"
+  fi   
+done < <(inotifywait -mr -e create -e moved_to $directory)
